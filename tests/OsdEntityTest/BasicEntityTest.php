31bdcea02ef2cdb339ada8a1bee671280d11309b
Angus <donald.macisaac@mail.mcgill.ca>
2015-03-03 09:03:52 -0500

    More tests

--- tests/OsdEntityTest/BasicEntityTest.php
+++ tests/OsdEntityTest/BasicEntityTest.php
@@ -31,6 +31,42 @@ class BasicEntityTest extends \PHPUnit_Framework_TestCase
         }
     }
 
+    /*
+     * @test
+     */
+    public function test_can_create_an_instance()
+    {
+        $data = $this->getData();
+
+        $entity = \EntityTest::create($data);
+
+        $entity = $entity->toArray();
+
+        foreach ($data as $key => $value) {
+            $this->assertEquals($entity[$key], $value);
+        }
+    }
+
+    /*
+     * @test
+     */
+    public function test_can_update_an_instance()
+    {
+        $data = $this->getData();
+
+        $entity = \EntityTest::create($data);
+
+        $updateData = $this->getUpdateData();
+
+        $entity->update($updateData);
+
+        $entity = $entity->toArray();
+
+        foreach ($updateData as $key => $value) {
+            $this->assertEquals($entity[$key], $value);
+        }
+    }
+
     protected function getData()
     {
         return array(
@@ -39,6 +75,17 @@ class BasicEntityTest extends \PHPUnit_Framework_TestCase
             'attributeThree' => 'testthree',
             'attribute_number_four' => 'testfour',
             'attributeNumberFive' => 'testFive',
+        );
+    }
+
+    protected function getUpdateData()
+    {
+        return array(
+            'attribute_one' => 'updatedone',
+            'attributeTwo' => 'updatedtwo',
+            'attributeThree' => 'updatedthree',
+            'attribute_number_four' => 'updatedfour',
+            'attributeNumberFive' => 'updatedfive',
         );
     }
 }
